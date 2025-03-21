ac42ca1d576e8e1954560dc3d2aeb8893fa24d33
thiii123 <canoahttp@gmail.com>
2025-03-21 00:16:57 -0300

    menu novo

---
+++ javasclipt.js
@@ -0,0 +1,8 @@
+function Menucell() {
+    var menu = document.getElementById("menu3");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
